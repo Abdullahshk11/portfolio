4e163345a9a4e4d700c3ef9fa429d10aa26b9e8e
Abdullahshk11 <abdullahshk4082>
2026-03-19 16:20:20 +0500

    Add projects

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+let tl = gsap.timeline();
+tl.from("header",
+    {
+        y: -100,
+        opacity: 0,
+        duration: 1
+    })
+tl.from(".about_lines, #element", {
+    y: 25,
+    opacity: 0,
+    duration: 0.3,
+    stagger: 0.1
+});
+tl.from(".about_pic", {
+    y: 20,
+    opacity: 0,
+    duration: 1
+});
 
 
 
-
-
-
-
-let btn = document.querySelector(".btn");
-btn.addEventListener("click", function(event) {
+let btn = document.getElementById("btn");
+btn.addEventListener("click", function (event) {
     event.preventDefault();
     let email = document.getElementById("E-mail").value;
     let msg = document.getElementById("msg").value;
-    if (email === "" ){
+    if (email === "") {
         alert("Please enter your email address.😑");
     } else if (msg === "") {
         alert("Please enter your message.🙄");
